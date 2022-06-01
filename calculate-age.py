5252e20a8231d557ad7e4f9b261beaff71cd4768
Cassie Rosa <rosac25@greenhill.org>
2022-06-01 13:02:16 -0500

    last commit

---
+++ calculate-age.py
@@ -0,0 +1,13 @@
+#Cassie Rosa
+#calculating age 
+
+import os 
+os .system('cls')
+
+born= int(input("year you were born: ")) #person can type year born in terminal
+current= int(input("current year: ")) #person can type the current year
+
+subtract= current - born 
+print(subtract) #displays answer
+
+if 
