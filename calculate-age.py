fb178696d9d04ad7bff82c7af0a89d68cd9dc0a9
Cassie Rosa <rosac25@greenhill.org>
2022-06-01 13:26:53 -0500

    commit again

--- calculate-age.py
+++ calculate-age.py
@@ -10,3 +10,8 @@ current= int(input("current year: ")) #person can type the current year
 subtract= current - born 
 print(subtract) #displays answer
 
+if (subtract > 61):
+    print (" your olddddd! heh")
+
+if (subtract  < 60 ): #how do equal too
+        print ("your a youngy")
