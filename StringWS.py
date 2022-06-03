3ec3e2d54e9a28556befe0ceb9deddf73a87352a
Cassie Rosa <rosac25@greenhill.org>
2022-06-03 11:20:29 -0500

    better

--- StringWS.py
+++ StringWS.py
@@ -21,24 +21,26 @@ print (message[wordlength-1]) #this will give the s
 #Exercise 1B: Create a string made of the middle three characters-- JhonDipPeta ->dip 
 
 message2= "JhonDipPeta"
-print (message2[4],end="")
+
 
 wordlength2= len(message2)  
 middle2 = int(wordlength2/2) #how to print middle character in message  (D)
+print (message2[middle2 -1], end="")
 print (message2[middle2], end="")
+print (message2[middle2+1], )
 
-print (message2[6])
 
 #Part 2 Exersise 1B: JaSonAy ->Son
 
 message3= "JaSonAy"
-print (message3[2],end="")
-
+#print (message3[2],end="")
+#print (message3[middle3 -1], end="")
 wordlength3= len(message3)  
 middle3 = int(wordlength3/2) #how to print middle character in message  (D)
+print (message3[middle3 -1], end="")
 print (message3[middle3], end="")
-
-print (message3[4],)
+print (message3[middle3 +1], )
+#print (message3[4],)
 
 
 #Exercise 2: Append new string in the middle of a given string
@@ -48,7 +50,7 @@ string2="kelly"
 
 wordlength4= len(string1)
 middle4= int(wordlength4/2)
-print(string1[0:middle]+string2 +string1[middle:])
+print(string1[0:middle]+string2 +string1[middle:]) #splits ault by using letters 0 to middle
 
 #Exercise 3: Create a new string made of the first, middle, and last characters of each input string
 
@@ -61,10 +63,10 @@ wordlength5= len(string3)
 wordlength6=len(string4)
 middle5= int(wordlength5/2)
 middle6=int(wordlength6/2)
-print (string3[3],end="")
+print (string3[middle5 -1],end="")
 #print (string3[middle],end="") 
 #print (string4[middle],end="") #middle of japan (p)
-print (string4[middle:])
+print (string4[middle6:])
 
 #Exercise 4: Arrange string characters such that lowercase letters should come first --> PyNaTive to yaivePNT
 
