6f9452a82f6f3630ba1d626c5bca5d09ec07f844
Cassie Rosa <rosac25@greenhill.org>
2022-06-03 11:37:22 -0500

    final

--- IntegerAndFloat.py
+++ IntegerAndFloat.py
@@ -6,3 +6,22 @@
 import os 
 os .system('cls')
 
+num=int(input("any integer")) #can imput in terminal any integer (whole number)
+
+if num % 2 ==0: #this checks if even 
+    print("Integer is even")
+else: print("Integer is odd")
+
+if num % 3 == 0: #this checks if multiple of 3 becasue there would be no remainder
+    print("this integer is a multiple of 3")
+
+if num % 5 ==0: #this checks if mulitple of 5 becasue there no remainder
+    print ("this integer is multiple of 5")
+
+if num % 3 == 0 and num % 5 ==0: #this checks if integer is both miltiple 5 and 3 (15)
+    print("this integer is a multiple of both")
+
+print(num%10) #this finds last digit of a number 
+print(num%100)
+print(num%1000)
+
