141af600abd4f55e5e9557918621ee8823c77f55
Cassie Rosa <rosac25@greenhill.org>
2022-06-03 10:51:11 -0500

    Final

---
+++ IntegerAndFloat.py
@@ -0,0 +1,8 @@
+#Cassie Rosa
+# integer and float work
+
+#Write a code that find if the number input by the user is even or odd is the number is multiple of 3 or 5
+
+import os 
+os .system('cls')
+
